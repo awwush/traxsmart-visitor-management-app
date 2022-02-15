2865f46f70c71c5abf585fbccc76c48ba96e3f7e
Tanish Pradhan <tanish.pradhan4@gmail.com>
2022-02-15 11:24:29 +0530

    Login + Forgot Password

---
+++ lib/ui/forgot_password.dart
@@ -0,0 +1,266 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:otp_text_field/otp_field.dart';
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({Key? key}) : super(key: key);
+
+  @override
+  _ForgotPasswordState createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  final List<bool> _dataExpansionPanel = [true, false];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 2.5 / 10,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                        color: const Color(0xffc5558e).withAlpha(100),
+                        borderRadius: const BorderRadius.only(
+                            bottomLeft: Radius.circular(96))),
+                  ),
+                  Positioned(
+                      top: 20,
+                      right: 30,
+                      child: GestureDetector(
+                        child: Row(
+                          children: [
+                            const Icon(
+                              Icons.vpn_lock_rounded,
+                              size: 17,
+                            ),
+                            const SizedBox(width: 4.0),
+                            Text(
+                              'en',
+                              style: GoogleFonts.lato(
+                                  textStyle: const TextStyle(
+                                fontSize: 14.0,
+                              )),
+                            ),
+                          ],
+                        ),
+                      )),
+                  Positioned(
+                    bottom: 20,
+                    right: 40,
+                    child: Text(
+                      "Forgot PIN",
+                      style: GoogleFonts.lato(
+                          textStyle:
+                              const TextStyle(fontWeight: FontWeight.w600),
+                          fontSize: 18.0,
+                          color: const Color(0xffc5558e)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                margin: const EdgeInsets.only(top: 32, left: 16, right: 16),
+                child: ExpansionPanelList(
+                  expandedHeaderPadding: const EdgeInsets.all(0),
+                  expansionCallback: (int index, bool isExpanded) {
+                    setState(() {
+                      _dataExpansionPanel[index] = !isExpanded;
+                    });
+                  },
+                  animationDuration: const Duration(milliseconds: 500),
+                  children: <ExpansionPanel>[
+                    ExpansionPanel(
+                        canTapOnHeader: true,
+                        headerBuilder: (BuildContext context, bool isExpanded) {
+                          return Container(
+                              padding: const EdgeInsets.all(16),
+                              child: Text(
+                                "Number",
+                                style: GoogleFonts.lato(
+                                    textStyle: TextStyle(
+                                        fontWeight: isExpanded
+                                            ? FontWeight.w600
+                                            : FontWeight.w400)),
+                              ));
+                        },
+                        body: Container(
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                            children: <Widget>[
+                              TextFormField(
+                                style: GoogleFonts.lato(
+                                    letterSpacing: 0.1,
+                                    color: const Color(0xffc5558e),
+                                    fontWeight: FontWeight.w500),
+                                decoration: InputDecoration(
+                                  prefixText: "+91 ",
+                                  prefixStyle: GoogleFonts.lato(
+                                      letterSpacing: 0.1,
+                                      color: const Color(0xffc5558e),
+                                      fontWeight: FontWeight.w500),
+                                  border: const OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(8.0),
+                                      ),
+                                      borderSide: BorderSide.none),
+                                  enabledBorder: const OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(8.0),
+                                      ),
+                                      borderSide: BorderSide.none),
+                                  focusedBorder: const OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(8.0),
+                                      ),
+                                      borderSide: BorderSide.none),
+                                  filled: true,
+                                  fillColor: const Color(0xfff0f0f0),
+                                  prefixIcon: const Icon(
+                                    Icons.phone,
+                                    size: 22,
+                                    color: Color(0xffc5558e),
+                                  ),
+                                  isDense: true,
+                                  contentPadding: const EdgeInsets.all(0),
+                                ),
+                                keyboardType: TextInputType.number,
+                                autofocus: true,
+                                textCapitalization:
+                                    TextCapitalization.sentences,
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(top: 16),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: <Widget>[
+                                    TextButton(
+                                        onPressed: () {},
+                                        child: Text(
+                                          "Call Me",
+                                          style: GoogleFonts.lato(
+                                              textStyle: const TextStyle(
+                                                  fontWeight: FontWeight.w600,
+                                                  color: Colors.grey)),
+                                        )),
+                                    Container(
+                                      decoration: const BoxDecoration(
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(4.0))),
+                                      child: TextButton(
+                                          onPressed: () {},
+                                          child: Text(
+                                            "Send OTP",
+                                            style: GoogleFonts.lato(
+                                                fontWeight: FontWeight.w600,
+                                                color: const Color(0xffc5558e)),
+                                          )),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        isExpanded: _dataExpansionPanel[0]),
+                    ExpansionPanel(
+                        canTapOnHeader: true,
+                        headerBuilder: (BuildContext context, bool isExpanded) {
+                          return Container(
+                              padding: const EdgeInsets.all(16),
+                              child: Text(
+                                "OTP",
+                                style: GoogleFonts.lato(
+                                    textStyle: TextStyle(
+                                        fontWeight: isExpanded
+                                            ? FontWeight.w600
+                                            : FontWeight.w500)),
+                              ));
+                        },
+                        body: Container(
+                            padding: const EdgeInsets.only(bottom: 16, top: 8),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 30.0),
+                                  child: OTPTextField(
+                                    length: 6,
+                                    width: MediaQuery.of(context).size.width,
+                                    textFieldAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    fieldWidth: 25,
+                                    outlineBorderRadius: 15,
+                                    style: const TextStyle(fontSize: 17),
+                                    onChanged: (pin) {
+                                      if (kDebugMode) {
+                                        print("Changed: " + pin);
+                                      }
+                                    },
+                                    onCompleted: (pin) {
+                                      if (kDebugMode) {
+                                        print("Completed: " + pin);
+                                      }
+                                    },
+                                  ),
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: <Widget>[
+                                    Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: <Widget>[
+                                        Text(
+                                          "Resend in ",
+                                          style: GoogleFonts.lato(
+                                              textStyle: const TextStyle(
+                                                  fontWeight: FontWeight.w600,
+                                                  letterSpacing: 0.2)),
+                                        ),
+                                        Text(
+                                          "00:59",
+                                          style: GoogleFonts.lato(
+                                              textStyle: const TextStyle(
+                                                  fontWeight: FontWeight.w500)),
+                                        ),
+                                      ],
+                                    ),
+                                    Container(
+                                      decoration: const BoxDecoration(
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(4.0))),
+                                      child: TextButton(
+                                          onPressed: () {},
+                                          child: Text(
+                                            "Verify",
+                                            style: GoogleFonts.lato(
+                                                textStyle: const TextStyle(
+                                                    color: Color(0xffc5558e))),
+                                          )),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            )),
+                        isExpanded: _dataExpansionPanel[1])
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
---
+++ lib/ui/login.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:otp_text_field/otp_field.dart';
+import 'package:vms/ui/forgot_password.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 3 / 10,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      color: const Color(0xffc5558e).withAlpha(100),
+                      borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(96))),
+                ),
+                Positioned(
+                    top: 20,
+                    right: 30,
+                    child: GestureDetector(
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.vpn_lock_rounded,
+                            size: 17,
+                          ),
+                          const SizedBox(width: 4.0),
+                          Text(
+                            'en',
+                            style: GoogleFonts.lato(
+                                textStyle: const TextStyle(
+                              fontSize: 14.0,
+                            )),
+                          ),
+                        ],
+                      ),
+                    )),
+                Positioned(
+                  bottom: 20,
+                  right: 40,
+                  child: Text(
+                    "Welcome to VSM",
+                    style: GoogleFonts.lato(
+                        textStyle: const TextStyle(fontWeight: FontWeight.w600),
+                        fontSize: 18.0,
+                        color: const Color(0xffc5558e)),
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 30.0),
+          Container(
+            padding: const EdgeInsets.fromLTRB(24, 24, 24, 8.0),
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.fromLTRB(15, 24, 15, 24),
+                  decoration: BoxDecoration(
+                    color: const Color(0xffc5558e).withAlpha(100),
+                    borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                        child: OTPTextField(
+                          length: 6,
+                          width: MediaQuery.of(context).size.width,
+                          textFieldAlignment: MainAxisAlignment.spaceAround,
+                          fieldWidth: 25,
+                          outlineBorderRadius: 15,
+                          style: const TextStyle(fontSize: 17),
+                          onChanged: (pin) {
+                            if (kDebugMode) {
+                              print("Changed: " + pin);
+                            }
+                          },
+                          onCompleted: (pin) {
+                            if (kDebugMode) {
+                              print("Completed: " + pin);
+                            }
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 40.0),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          decoration: const BoxDecoration(
+                              color: Color(0xffc5558e),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10.0))),
+                          child: const Icon(
+                            Icons.chevron_right_rounded,
+                            size: 40.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20.0),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) => const ForgotPassword()));
+                        },
+                        child: Text(
+                          'Forgot PIN?',
+                          style: GoogleFonts.lato(
+                              textStyle: const TextStyle(
+                                  color: Color(0xffc5558e), fontSize: 14.0)),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+}
